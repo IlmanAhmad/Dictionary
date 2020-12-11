e4398266b6dd5f2892921a0b31d57848a0c979b5
Ilman Ahmad <70219582+IlmanAhmad@users.noreply.github.com>
2020-12-11 21:05:00 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-#config.py
-headers = {
-        'x-rapidapi-key': "779dd1133fmsh583725ff7ccdd3ep184db9jsne5a954439efa",
-        'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com"
-        }
